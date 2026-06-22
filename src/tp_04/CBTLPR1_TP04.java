c1001c018e2e628f03ffd2baeb7f63738f5363cb
Rayssa Silva de Oliveira <143881380+RayssaOlv@users.noreply.github.com>
2026-06-22 13:57:49 -0300

    Update CBTLPR1_TP04.java

--- src/tp_04/CBTLPR1_TP04.java
+++ src/tp_04/CBTLPR1_TP04.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tp_04;
-
+import java.util.*;
 /**
  *
  *  Nomes: João Victor Lima Venceslau - Rayssa Silva de Oliveira.
